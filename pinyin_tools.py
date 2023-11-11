b1ad078855a3dc573e45b201e9c538b8282863a2
Honghao SHAN 单弘昊 <honghao.shan@nio.com>
2023-11-11 09:57:14 +0800

    feature support curtom dictionary

---
+++ pinyin_tools.py
@@ -0,0 +1,70 @@
+import os
+
+from pypinyin import pinyin, Style
+from pypinyin.style._utils import get_initials, get_finals
+
+_double_pinyin_dicts = {}
+
+
+def get_double_pinyin_dict(double_pinyin_type):
+    if not _double_pinyin_dicts.__contains__(double_pinyin_type):
+        _double_pinyin_dicts[double_pinyin_type] = {
+            'initials': create_dict(os.path.join('./double_pinyin', double_pinyin_type, 'initials.txt')),
+            'finals1': create_dict(os.path.join('./double_pinyin', double_pinyin_type, 'finals1.txt')),
+            'finals2': create_dict(os.path.join('./double_pinyin', double_pinyin_type, 'finals2.txt'))
+        }
+    return _double_pinyin_dicts[double_pinyin_type]
+
+
+def create_dict(textfile):
+    dict = {}
+    with open(textfile, "r", encoding="utf-8") as file:
+        for line in file:
+            if line.strip().startswith('#'):
+                continue
+            (key, value) = line.split()
+            dict[key] = value
+    # print(dict)
+    return dict
+
+
+def convert_double(record_dict, double_pinyin_type):
+    """
+    转换成双拼词库
+    :param record_dict:
+    :param double_pinyin_type:
+    :return:
+    """
+    double_pinyin_dict = get_double_pinyin_dict(double_pinyin_type)
+
+    results = {}
+    for cn, py_list in record_dict.items():
+        words = []
+        for py in py_list.split(' '):
+            word_initials = get_initials(py, False)
+            word_finals = get_finals(py, False)
+            if word_initials == '':
+                # 零声母
+                words.append(double_pinyin_dict['finals2'][word_finals])
+            else:
+                words.append(double_pinyin_dict['initials'][word_initials] + double_pinyin_dict['finals1'][word_finals])
+        results[cn] = ''.join(words)
+
+    return results
+
+
+def get_pinyin(word):
+    """
+    获取单个词的拼音
+    :param word:
+    :return:
+    """
+    pinyin_list = pinyin(word, style=Style.NORMAL, strict=False, heteronym=True)
+    multi_py = False
+    for py in pinyin_list:
+        if py.__len__() > 1:
+            multi_py = True
+
+    if multi_py:
+        print("词语存在多音字, 建议手动指定: %s" % word)
+    return ' '.join(list(map(lambda x: x[0], pinyin_list)))
--- scel2dict.py
+++ scel2dict.py
@@ -1,14 +1,13 @@
 import os
-import sys
 
-from scel_tools import get_records_from_scel
-from double_pinyin import convert_double
 from dict_writter import write_dict
+from pinyin_tools import convert_double, get_pinyin
+from scel_tools import get_records_from_scel
+
 
 def save_text(records, fout):
     records_translated = list(map(lambda x: "%s\t%s" % (x[1], x[0]), records))
     fout.write("\n".join(records_translated))
-
 
 
 def convert_dict(record_dict, double_pinyin_type, out_format):
@@ -26,6 +25,27 @@ def convert_dict(record_dict, double_pinyin_type, out_format):
     if out_format == 'gboard':
         write_dict(py_dict, './out', out_format, double_pinyin_type)
 
+def load_custom_dicts():
+    custom_files = list(filter(lambda x: x.endswith('.txt'), os.listdir('./custom')))
+    records = []
+    for custom_file in custom_files:
+        with open(os.path.join('./custom', custom_file), 'r', encoding='utf-8') as f:
+            lines = f.readlines()
+            lines = list(map(lambda x: x.strip(), lines))
+            lines = list(filter(lambda x: x != '', lines))
+            lines = list(filter(lambda x: not x.startswith('#'), lines))
+            lines = list(map(lambda x: x.split('|'), lines))
+
+            for line in lines:
+                if len(line) == 1:
+                    # 只写了中文, 没写拼音
+                    py = get_pinyin(line[0])
+                    records.append((py, line[0]))
+                else:
+                    # 写了中文和拼音
+                    records.append((line[1], line[0]))
+
+    return records
 
 def main():
     scel_files = list(filter(lambda x: x.endswith('.scel'), os.listdir('./scel')))
@@ -45,6 +65,9 @@ def main():
             save_text(records, fout)
             all_records.extend(records)
         print("-" * 80)
+
+    custom_records = load_custom_dicts()
+    all_records.extend(custom_records)
 
     record_dict = {item[1]: item[0] for item in all_records}  # 全量词库 dict
     print("合并后 %s 个词" % (len(record_dict)))
