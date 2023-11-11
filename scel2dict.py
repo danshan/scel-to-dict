57a8c03f8dd841a83c03990d11c0526ae23b7d8d
Honghao SHAN 单弘昊 <honghao.shan@nio.com>
2023-11-11 10:45:01 +0800

    feature add readme file

--- scel2dict.py
+++ scel2dict.py
@@ -25,32 +25,29 @@ def convert_dict(record_dict, double_pinyin_type, out_format):
     if out_format == 'gboard':
         write_dict(py_dict, './out', out_format, double_pinyin_type)
 
-def load_custom_dicts():
-    custom_files = list(filter(lambda x: x.endswith('.txt'), os.listdir('./custom')))
-    records = []
-    for custom_file in custom_files:
-        with open(os.path.join('./custom', custom_file), 'r', encoding='utf-8') as f:
-            lines = f.readlines()
-            lines = list(map(lambda x: x.strip(), lines))
-            lines = list(filter(lambda x: x != '', lines))
-            lines = list(filter(lambda x: not x.startswith('#'), lines))
-            lines = list(map(lambda x: x.split('|'), lines))
 
-            for line in lines:
-                if len(line) == 1:
-                    # 只写了中文, 没写拼音
-                    py = get_pinyin(line[0])
-                    records.append((py, line[0]))
-                else:
-                    # 写了中文和拼音
-                    records.append((line[1], line[0]))
+def get_records_from_custom(custom_file_path):
+    records = []
+    with open(custom_file_path, 'r', encoding='utf-8') as f:
+        lines = f.readlines()
+        lines = list(map(lambda x: x.strip(), lines))
+        lines = list(filter(lambda x: x != '', lines))
+        lines = list(filter(lambda x: not x.startswith('#'), lines))
+        lines = list(map(lambda x: x.split('|'), lines))
+
+        for line in lines:
+            if len(line) == 1:
+                # 只写了中文, 没写拼音
+                py = get_pinyin(line[0])
+                records.append((py, line[0]))
+            else:
+                # 写了中文和拼音
+                records.append((line[1], line[0]))
 
     return records
 
-def main():
-    scel_files = list(filter(lambda x: x.endswith('.scel'), os.listdir('./scel')))
-    scel_names = list(map(lambda x: x[:-5], scel_files))
 
+def main():
     if not os.path.exists("./out"):
         os.mkdir("./out")
     if not os.path.exists("./custom"):
@@ -58,21 +55,30 @@ def main():
 
     all_records = []  # 全量词库 list
 
+    # load scel dicts
+    scel_files = list(filter(lambda x: x.endswith('.scel'), os.listdir('./scel')))
     for scel_file in scel_files:
         scel_file_path = os.path.join("./scel", scel_file)
         records = get_records_from_scel(scel_file_path)
-        print("%s: %s 个词" % (scel_file, len(records)))
+        print("sougou 词库 [%s]: %s 个词" % (scel_file, len(records)))
 
         with open(os.path.join("./out", scel_file.replace(".scel", ".txt")), "w") as fout:
             save_text(records, fout)
             all_records.extend(records)
         print("-" * 80)
 
-    custom_records = load_custom_dicts()
-    all_records.extend(custom_records)
+    # load custom dicts
+    custom_files = list(filter(lambda x: x.endswith('.txt'), os.listdir('./custom')))
+    for custom_file in custom_files:
+        custom_file_path = os.path.join("./custom", custom_file)
+        records = get_records_from_custom(custom_file_path)
+        print("custom 词库 [%s]: %s 个词" % (custom_file, len(records)))
+
+        all_records.extend(records)
+        print("-" * 80)
 
     record_dict = {item[1]: item[0] for item in all_records}  # 全量词库 dict
-    print("合并后 %s 个词" % (len(record_dict)))
+    print(">> 合并后 %s 个词" % (len(record_dict)))
 
     convert_dict(record_dict, 'xiaohe', 'gboard')
 
